dcfccca5bd9724f9b0505da3c9c91b2c7b8b6bf6
Yusuf Said DURDURAN <yusufsd1996@gmail.com>
2024-06-03 14:12:26 +0300

    pizzarro

---
+++ script.js
@@ -0,0 +1,29 @@
+const searchForm = document.querySelector(".searchForm");
+const searchBtn = document.querySelector(".searchBtn");
+
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.getElementById("menu-Btn");
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) &&
+      !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
+
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+});
